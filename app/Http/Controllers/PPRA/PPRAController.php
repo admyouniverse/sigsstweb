053051da031622195181c4a0940c9ca0c05da054
Vitor Hugo Lopes <vitor@youniverseweb.com>
2021-02-20 09:58:48 -0300

    acertando versão

--- app/Http/Controllers/PPRA/PPRAController.php
+++ app/Http/Controllers/PPRA/PPRAController.php
@@ -22,7 +22,7 @@ class PPRAController extends Controller
      */
     public function index()
     {
-        return view('ppra.index', compact('ppras'));
+        return view('ppra.index');
     }
 
     /**
